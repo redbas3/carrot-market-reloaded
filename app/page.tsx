f96b6acf3850d9b268087b7695488e748db07247
RJTmini <redbas3@gmail.com>
2024-04-27 19:50:27 +0900

    #3.6 Form Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ export default function Home() {
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-3">
         <input
-          className="w-full rounded-full bg-gray-200 pl-5 py-2 outline-none ring ring-transparent focus:ring-orange-500 focus-ring-offset-2 transition-shadow"
-          type="text"
-          placeholder="Searh here..."
+          className="w-full rounded-full bg-gray-200 pl-5 py-2 outline-none ring ring-transparent focus:ring-orange-500 focus-ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
+          type="email"
+          placeholder="Email address"
+          required
         />
-        <button className="bg-black text-white px-5 py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none md:px-10">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required.
+        </span>
+        <button className="bg-black text-white px-5 py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none md:px-1">
+          Log in
         </button>
       </div>
     </main>
