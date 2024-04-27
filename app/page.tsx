d503f5f31d409fa3c4c964b7a0d9f477929a3659
RJTmini <redbas3@gmail.com>
2024-04-27 19:31:54 +0900

    #3.5 Responsive Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col gap-3">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-3">
         <input
           className="w-full rounded-full bg-gray-200 pl-5 py-2 outline-none ring ring-transparent focus:ring-orange-500 focus-ring-offset-2 transition-shadow"
           type="text"
           placeholder="Searh here..."
         />
-        <button className="bg-black text-white px-5 py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none">
+        <button className="bg-black text-white px-5 py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none md:px-10">
           Search
         </button>
       </div>
