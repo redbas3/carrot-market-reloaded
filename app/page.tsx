f698f35a770a61eb8dac524969aff9c1f3666589
RJTmini <redbas3@gmail.com>
2024-04-27 19:26:13 +0900

    #3.4 Tailwind Variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,15 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-500 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-3xl font-semibold dark:text-white">
-              Coolblue
-            </span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-sm font-medium rounded-full transition hover:bg-green-600 hover:scale-125">
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 w-full h-2 rounded-full absolute" />
-          <div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col gap-3">
+        <input
+          className="w-full rounded-full bg-gray-200 pl-5 py-2 outline-none ring ring-transparent focus:ring-orange-500 focus-ring-offset-2 transition-shadow"
+          type="text"
+          placeholder="Searh here..."
+        />
+        <button className="bg-black text-white px-5 py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none">
+          Search
+        </button>
       </div>
     </main>
   );
