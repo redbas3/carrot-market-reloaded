37c12b471cf12795ff559594a6911ba7a5bb7e77
RJTmini <redbas3@gmail.com>
2024-04-30 01:46:55 +0900

    #6.1 Validation Errors

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,7 +2,12 @@
 
 import { z } from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10)
+});
 
 export const createAccount = async (prevState: any, formData: FormData) => {
   const data = {
@@ -11,8 +16,9 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  console.log(data);
-  
-  usernameSchema.parse(data.username);
-  
+
+  const result = formSchema.safeParse(data);
+  if(!result.success) {
+    return result.error.flatten();
+  }
 };
