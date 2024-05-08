6338320839eda4375e118d1d177b2d0ef82e780a
RJTmini <redbas3@gmail.com>
2024-05-09 00:41:04 +0900

    #8.7 Log Out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,43 @@
-export default function Profile() {
-  return <h1>welcome to your profile</h1>;
+import { PrismaClient } from "@prisma/client";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+const db = new PrismaClient();
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+
+    const session = await getSession();
+    await session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>welcome {user?.username}</h1>
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
