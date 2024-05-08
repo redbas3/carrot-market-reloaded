0d56141114766617c93b564ccfdfb0dcdc0a76a5
RJTmini <redbas3@gmail.com>
2024-05-08 21:52:15 +0900

    #8.1 Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,9 +2,35 @@
 
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWROD_REGEX_ERROR } from "@/lib/constants";
 import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
 
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPasswords = ({password, confirm_password}: {password: string, confirm_password: string}) => password === confirm_password;
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+}
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+}
+
 
 const formSchema = z.object({
   username: z.string({
@@ -15,8 +41,10 @@ const formSchema = z.object({
   .toLowerCase()
   .trim()
   .transform((data) => data.replace(/\s/g, "-")) // Replace spaces with dashes
-  .refine(checkUsername, "No potatoes allowed!"),
-  email: z.string().email().toLowerCase(),
+  .refine(checkUsername, "No potatoes allowed!")
+  .refine(checkUniqueUsername, "This username is already taken!"),
+  email: z.string().email().toLowerCase()
+  .refine(checkUniqueEmail, "There is an account already registered with this email!"),
   password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWROD_REGEX_ERROR),
   confirm_password: z.string().min(10)
 }).refine(checkPasswords, {
@@ -32,11 +60,15 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if(!result.success) {
     return result.error.flatten();
   }
   else {
-    console.log(result.data);
+    // hash pasword
+    // save the user to db
+    // log the user in
+    // redirect to home
+
   }
 };
