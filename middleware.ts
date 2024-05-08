af714da76599daba08262459c9034917e3688c8d
RJTmini <redbas3@gmail.com>
2024-05-09 01:46:06 +0900

    #8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if(pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("hello", "world");
-    return response;
-  }
-
-  if(pathname === "/profile") {
-    const session = await getSession();
-    if(!session.id) {
-      return Response.redirect(new URL("/login", request.url));
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if(!session.id) {
+    if(!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
     }
-
-    // return Response.json({ error: "you are not allowed here!" });
-    // return Response.redirect(new URL("/", request.url));
+  }
+  else {
+    if(exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
 export const config = {
-  // matcher:["/", "/profile", "/create-account", "/login", "/user/:path*"]
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 }
