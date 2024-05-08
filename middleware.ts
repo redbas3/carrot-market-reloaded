f6f6059e0f7777c3d44050cc412ab6c10635f477
RJTmini <redbas3@gmail.com>
2024-05-09 01:34:05 +0900

    #8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,26 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  if(request.nextUrl.pathname === "/profile") {
+  const pathname = request.nextUrl.pathname;
+  if(pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("hello", "world");
+    return response;
+  }
+
+  if(pathname === "/profile") {
     const session = await getSession();
     if(!session.id) {
-      console.log(request.url);
       return Response.redirect(new URL("/login", request.url));
     }
 
     // return Response.json({ error: "you are not allowed here!" });
     // return Response.redirect(new URL("/", request.url));
   }
+}
+
+export const config = {
+  // matcher:["/", "/profile", "/create-account", "/login", "/user/:path*"]
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 }
