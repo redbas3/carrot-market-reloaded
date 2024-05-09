2a2ea2c6a4111fc845c9c66dade9c107af3eced6
RJTmini <redbas3@gmail.com>
2024-05-09 16:58:48 +0900

    #10.3 Product Component

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,12 +1,30 @@
+import ListProduct from "@/components/list-product";
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+
+  return products;
 }
 
 export default async function Products() {
   const products = await getProducts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
