2a2eaa6e8978f593106dd78b10b5ec22db08dcae
RJTmini <redbas3@gmail.com>
2024-05-11 11:59:36 +0900

    #10.7 Pagination Actions

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,5 +1,6 @@
-import ListProduct from "@/components/list-product";
+import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
 async function getProducts() {
   const products = await db.product.findMany({
@@ -10,18 +11,22 @@ async function getProducts() {
       photo: true,
       id: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
   });
 
   return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<typeof getProducts>;
+
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getProducts();
   return (
-    <div className="p-5 flex flex-col gap-5">
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+    <div>
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
---
+++ components/product-list.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { InitialProducts } from "@/app/(tabs)/products/page";
+import ListProduct from "./list-product";
+import { useState } from "react";
+import { getMoreProducts } from "@/app/(tabs)/products/actions";
+
+interface ProductListProps {
+  initialProducts: InitialProducts;
+}
+
+export default function ProductList({ initialProducts }: ProductListProps) {
+  const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const onLoadMoreClick = async () => {
+    setIsLoading(true);
+    const newProducts = await getMoreProducts(products.length);
+    setProducts((prev) => [...prev, ...newProducts]);
+    setIsLoading(false);
+  };
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+      <button
+        onClick={onLoadMoreClick}
+        disabled={isLoading}
+        className="text-sm font-semibold rounded-md bg-orange-500 text-white px-3 py-2 mx-auto hover:opacity-90 active:scale-95"
+      >
+        {isLoading ? "로딩중" : "Load more"}
+      </button>
+    </div>
+  );
+}
