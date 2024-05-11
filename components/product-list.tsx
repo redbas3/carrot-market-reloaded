ee3348c16241b0aef8de9ad65af0db3f28578af6
redbas3 <redbas3@gmail.com>
2024-05-11 15:03:54 +0900

    #10.9 Infinite Scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -2,7 +2,7 @@
 
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import ListProduct from "./list-product";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 
 interface ProductListProps {
@@ -13,7 +13,38 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
-  const [lastPage, setLastPage] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
+
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      const element = entries[0];
+      if (element.isIntersecting && trigger.current) {
+        observer.unobserve(trigger.current);
+        setIsLoading(true);
+        const newProducts = await getMoreProducts(page + 1);
+        if (newProducts.length !== 0) {
+          setPage(prev => prev + 1);
+          setProducts((prev) => [...prev, ...newProducts]);
+        }
+        else {
+          setIsLastPage(true);
+        }
+        setIsLoading(false);
+      }
+    }, {
+      threshold: 1.0,
+      rootMargin: "0px 0px -100px 0px"
+    });
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+    return () => {
+      observer.disconnect();
+    }
+  }, [page]);
+
   const onLoadMoreClick = async () => {
     setIsLoading(true);
     const newProducts = await getMoreProducts(page + 1);
@@ -22,7 +53,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       setProducts((prev) => [...prev, ...newProducts]);
     }
     else {
-      setLastPage(true);
+      setIsLastPage(true);
     }
     setIsLoading(false);
   };
@@ -31,13 +62,16 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {!lastPage ? <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold rounded-md bg-orange-500 text-white px-3 py-2 mx-auto hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? "로딩중" : "Load more"}
-      </button> : null}
+      {!isLastPage ?
+        <span
+          ref={trigger}
+          style={{
+            marginTop: `${page + 1 * 200}vh`,
+          }}
+          className="mb-96 text-sm font-semibold rounded-md bg-orange-500 text-white px-3 py-2 mx-auto hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩중" : "Load more"}
+        </span> : null}
     </div>
   );
 }
