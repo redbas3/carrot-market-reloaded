40377f497cc6179ab7ebe85daca64c51062c049d
RJTmini <redbas3@gmail.com>
2024-05-14 03:56:08 +0900

    #12.1 Intercepting Routes

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { InitialProducts } from "@/app/(tabs)/products/page";
+import { InitialProducts } from "@/app/(tabs)/home/page";
 import ListProduct from "./list-product";
 import { useEffect, useRef, useState } from "react";
-import { getMoreProducts } from "@/app/(tabs)/products/actions";
+import { getMoreProducts } from "@/app/(tabs)/home/actions";
 
 interface ProductListProps {
   initialProducts: InitialProducts;
@@ -18,41 +18,45 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const trigger = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-      const element = entries[0];
-      if (element.isIntersecting && trigger.current) {
-        observer.unobserve(trigger.current);
-        setIsLoading(true);
-        const newProducts = await getMoreProducts(page + 1);
-        if (newProducts.length !== 0) {
-          setPage(prev => prev + 1);
-          setProducts((prev) => [...prev, ...newProducts]);
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
         }
-        else {
-          setIsLastPage(true);
-        }
-        setIsLoading(false);
+      },
+      {
+        threshold: 1.0,
+        rootMargin: "0px 0px -100px 0px",
       }
-    }, {
-      threshold: 1.0,
-      rootMargin: "0px 0px -100px 0px"
-    });
+    );
     if (trigger.current) {
       observer.observe(trigger.current);
     }
     return () => {
       observer.disconnect();
-    }
+    };
   }, [page]);
 
   const onLoadMoreClick = async () => {
     setIsLoading(true);
     const newProducts = await getMoreProducts(page + 1);
     if (newProducts.length !== 0) {
-      setPage(prev => prev + 1);
+      setPage((prev) => prev + 1);
       setProducts((prev) => [...prev, ...newProducts]);
-    }
-    else {
+    } else {
       setIsLastPage(true);
     }
     setIsLoading(false);
@@ -62,13 +66,14 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {!isLastPage ?
+      {!isLastPage ? (
         <span
           ref={trigger}
           className="text-sm font-semibold rounded-md bg-orange-500 text-white px-3 py-2 mx-auto hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩중" : "Load more"}
-        </span> : null}
+        </span>
+      ) : null}
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -17,18 +17,17 @@ const publicOnlyUrls: Routes = {
 export async function middleware(request: NextRequest) {
   const session = await getSession();
   const exists = publicOnlyUrls[request.nextUrl.pathname];
-  if(!session.id) {
-    if(!exists) {
+  if (!session.id) {
+    if (!exists) {
       return NextResponse.redirect(new URL("/", request.url));
     }
-  }
-  else {
-    if(exists) {
-      return NextResponse.redirect(new URL("/products", request.url));
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/home", request.url));
     }
   }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
