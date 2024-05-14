13aa73a8d4f781d60ac15f3f21ed3b99ba8c6f88
RJTmini <redbas3@gmail.com>
2024-05-14 10:11:27 +0900

    #12.5 Modal Route

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -29,7 +29,10 @@ export default async function Products() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
-      <Link href="/products/add" className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-6 text-white transition-colors hover:bg-orange-400">
+      <Link
+        href="/products/add"
+        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-6 text-white transition-colors hover:bg-orange-400"
+      >
         <PlusIcon className="size-10" />
       </Link>
     </div>
