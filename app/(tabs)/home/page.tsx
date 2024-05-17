9fcf15c4eb596980131d1131d031d66661e1d7bf
RJTmini <redbas3@gmail.com>
2024-05-18 01:56:06 +0900

    #13.7 Route Segment Config

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -29,6 +29,8 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
+export const revalidate = 60;
+
 export default async function Products() {
   const initialProducts = await getCashedProducts();
   const revalidate = async () => {
